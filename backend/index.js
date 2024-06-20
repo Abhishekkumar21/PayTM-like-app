0489fe6eed42d537d576f1c98d6fb8beec2b9e8d
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-06-20 23:38:21 +0530

    connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+//loading environment variable from .env file
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//MongoDB connection
+const dbURI = process.env.MONGODB_URI;
+mongoose
+  .connect(dbURI)
+  .then(() => console.log("Connected to MongoDB. Database created"))
+  .catch((err) => console.log("Failed to connect MongoDB", err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
