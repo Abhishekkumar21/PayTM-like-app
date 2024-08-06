b9d7c99235a3527f83ef76774c048a3837174f0a
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-08-06 13:11:35 +0530

    Added : Models for User Model

---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+//creating schema for user data to be stored in MongoDB collections
+const userSchema = mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+//creating mongoose model 'User', after the creating of user schema
+const User = mongoose.model("User", userSchema);
+export default User; //exporting the 'User' model so it can be used in other parts of the application.
