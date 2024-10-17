c2c48b4aeff86d43bbcac98835fd80775433bc66
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-10-17 10:22:22 +0530

    Created db models for Account info

---
+++ backend/models/account.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import User from "./user.model.js";
+
+// creating schema for Accounts
+const accountSchema = new mongoose.Schema({
+  userID: {
+    type: mongoose.Schema.Types.ObjectId, // reference to Model: User
+    ref: User,
+    required: true,
+  },
+  balance: { type: Number, required: true },
+});
+
+//creating model w.r.t account schema
+
+const Account = mongoose.model("Account", accountSchema);
+export default Account;
