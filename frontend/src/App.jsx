3f0bdc90d1efc35eb204144e63f20b28e7fb1fde
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-10-18 23:59:18 +0530

    initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
-
+import React from "react";
 function App() {
-
-  return (
-    <div>
-        Hello world
-    </div>
-  )
+  return <div>Hello world</div>;
 }
 
-export default App
+export default App;
